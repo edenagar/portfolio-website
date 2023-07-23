fca66fd7decb3615526358ef7a1bda7d3809e90f
edenagar <nagar.eden6@gmail.com>
2023-07-23 15:23:18 +0300

    Added current project

--- src/home/ProjectGrid.js
+++ src/home/ProjectGrid.js
@@ -6,37 +6,25 @@ import Box from '@mui/material/Box';
 
 const projects = [
   {
-    title: 'Project 1',
-    description: 'This is the first project.',
-    image: 'project1.jpg', // Replace with the image path
+    title: 'Sicher Mapping',
+    description: 'Company website.',
+    image: 'https://www.sicher-mapping.com/static/media/sicher_navbar_logo.a4844a22.webp.',
   },
   {
-    title: 'Project 2',
-    description: 'This is the second project.',
-    image: 'project2.jpg', // Replace with the image path
+    title: 'T:hub Technion',
+    description: 'Inovation day Website CRM.',
+    image: 'https://scontent-lhr8-2.xx.fbcdn.net/v/t39.30808-1/320481699_1826323404390922_2032634562119185305_n.jpg?stp=dst-jpg_p200x200&_nc_cat=104&ccb=1-7&_nc_sid=c6021c&_nc_ohc=Z2B0j_t06vEAX_X_-qA&_nc_ht=scontent-lhr8-2.xx&oh=00_AfDHWZS8NAQYRhyccXYm_oD5x63sBoJZYo9O8RPRcyqygA&oe=64C24C9B', // Replace with the image path
   },
   {
-    title: 'Project 3',
-    description: 'This is the third project.',
-    image: 'project3.jpg', // Replace with the image path
+    title: 'DNA Sequencing Algorithm', 
+    description: 'Under the supervision of Prof. Lamm.',
+    image: "https://biology.technion.ac.il/wp-content/themes/BiologyAcc/images/logo-en.png",
   },
   {
-    title: 'Project 1',
-    description: 'This is the first project.',
-    image: 'project1.jpg', // Replace with the image path
+    title: 'Kravi Tech',
+    description: 'Mentored a face recognition project.',
+    image: 'https://media.licdn.com/dms/image/D4D22AQFWpF3sCWp-pQ/feedshare-shrink_800/0/1686665936085?e=1692835200&v=beta&t=HwKaEzENSz5T6y2bKE0KtRWWH48ZTwm7Toh_6Z1qdWY', // Replace with the image path
   },
-  {
-    title: 'Project 2',
-    description: 'This is the second project.',
-    image: 'project2.jpg', // Replace with the image path
-  },
-  {
-    title: 'Project 3',
-    description: 'This is the third project.',
-    image: 'project3.jpg', // Replace with the image path
-  },
-  
-  // Add more projects here
 ];
 
 const ProjectGrid = () => {
@@ -48,7 +36,8 @@ const ProjectGrid = () => {
             sx={{
               position: 'relative',
               backgroundImage: `url(${project.image})`,
-              backgroundSize: 'cover',
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
               backgroundPosition: 'center',
               height: '200px',
               borderRadius: 0,
