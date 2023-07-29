637a3dc90edcdc9fced22662a83f87b2199fbdb9
edenagar <nagar.eden6@gmail.com>
2023-07-29 15:00:39 +0300

    Moving layout to a diffrent folder

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Navbar from './Navbar'
 import Intreduction from './Intreduction'
 import HorizontalTimeline from './Timeline'
 import ProjectGrid from './ProjectGrid'
-import Footer from './Footer'
+import Navbar from '../../layouts/Navbar'
+import Footer from '../../layouts/Footer'
 import Contribution from './Contribution'
 
 
@@ -19,7 +19,6 @@ const Home = () => {
         <Intreduction />
         <HorizontalTimeline />
         <Contribution />
-        
         <ProjectGrid />
       </main>
       <Footer />
