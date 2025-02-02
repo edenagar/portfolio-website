58ec61d823a0b44a29b5a1cdc99371963ed26a11
edenagar <nagar.eden6@gmail.com>
2025-02-02 12:31:35 +0200

    Remove testing libraries, project grid, and cleanup project configuration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css';
 import Home from './pages/home/Home';
-import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/portfolio-website/",
+    path: "/",
     element: <Home />,
   },
-
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,8 +20,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
